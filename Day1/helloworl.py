466e39b8dcaec889f04c7945aae24ab8112e0d0b
musbah1234 <musbahuumaryakubu@gmail.com>
2023-12-15 22:00:25 +0100

    day1 assignment

---
+++ Day1/helloworl.py
@@ -0,0 +1,28 @@
+# Introduction
+
+# Day1 30days-of-python challange
+
+print(3 + 4)  #Addition
+print(3 - 4)  #Subtraction
+print(3 * 4)  #Multiplication
+print(3 / 4)  #Division
+print(3 ** 4) #Exponetial
+print(3 % 4)  #mudulus
+print(3 // 4) #Flow division operator
+
+
+print("Musbahu") 
+print("Umar")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+
+#Writing data type
+
+print(type(10))                                 #int
+print(type(9.8))                                #float
+print(type(3.14))                               #float
+print(type(4 - 4j))                             #complex
+print(type(['musbah','python','Nigeria']))      #list
+print(type('Musbah'))                           #string
+print(type('Umar'))                             #string
+print(type('Nigeria'))                          #string
